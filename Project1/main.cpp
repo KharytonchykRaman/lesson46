b0486b148b6d3054ad9d6079b24596153c195bac
Danila <ddanilsachko@gmail.com>
2024-03-06 18:15:47 +0300

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 5;
+	int m = 10;
+
+	int** classes = new int*[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		classes[i] = new int[m];
+	}
+
+	// code
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] classes[i];
+	}
+	delete[] classes;
+
+	return 0;
+}
